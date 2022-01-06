789e74c764d7f301b113d26c4d7d335bbad9f1b3
Musa <77996753+MusaAqeel@users.noreply.github.com>
2022-01-06 02:06:35 -0500

    Add files via upload
    Final Website - Server problems led us to upload this now rather than before as we could not upload final code at that time.

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  
+    $(window).scroll(function () {
+        //if you hard code, then use console
+        //.log to determine when you want the 
+        //nav bar to stick.  
+        console.log($(window).scrollTop())
+      if ($(window).scrollTop() > 280) {
+        $('#nav_bar').addClass('navbar-fixed');
+      }
+      if ($(window).scrollTop() < 281) {
+        $('#nav_bar').removeClass('navbar-fixed');
+      }
+    });
+  });
+
